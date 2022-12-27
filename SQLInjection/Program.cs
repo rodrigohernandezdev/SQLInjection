095ee10d560979fb241cda78c5ef016eb52ec4af
rodrigohernandezdev <rodrigo.hernandezdev@gmail.com>
2022-12-27 16:08:32 -0600

    first commit

---
+++ SQLInjection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Server=localhost;Database=test;User Id=user;Password=password;";
+        string userInput = Console.ReadLine();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT * FROM users WHERE username='" + userInput + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["username"] + " - " + reader["email"]);
+            }
+
+            connection.Close();
+        }
+    }
+}
